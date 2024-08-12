6f32e05629fa9d5310d22df41ee7bf8f950245b6
Vivek Sawant <vicky210490@gmail.com>
2024-08-12 16:16:00 +0530

    assign handlers to routes

--- routes/url.js
+++ routes/url.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const { handleGenerateNewShortUrl, handleRedirect, handleAnalytics } = require('../controllers/url')
 
 const router = express.Router()
 
-router.post('/')
+router.post('/', handleGenerateNewShortUrl)
+router.get('/analytics/:shortId', handleAnalytics)
+router.route('/:shortId')
+    .get(handleRedirect)
+
+module.exports = router
