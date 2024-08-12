6ee461878468d131103144495b6ccf62f4509851
Vivek Sawant <vicky210490@gmail.com>
2024-08-12 14:53:46 +0530

    git init

---
+++ index.js
@@ -0,0 +1,12 @@
+// imports
+const express = require('express')
+
+// create app
+const app = express()
+
+// define port
+const PORT = 8001
+
+// assign port to app
+app.listen(PORT, () => console.log(`app started on port: ${PORT}`);
+ )
